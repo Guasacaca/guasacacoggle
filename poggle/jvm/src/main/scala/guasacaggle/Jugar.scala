fc766cfc70104804d6219ed36d00f998fe5022c7
Marilyn Nowacka <marilyn.nowacka@fyber.com>
2018-09-14 13:34:53 +0200

    add gitignore

---
+++ poggle/jvm/src/main/scala/guasacaggle/Jugar.scala
@@ -0,0 +1,29 @@
+package guasacaggle
+
+import language.Spanish
+import poggle.Preparation.{Board, Game}
+
+object Jugar extends App {
+
+  def continue(writeIn: String): Boolean = {
+    writeIn match {
+      case "c" => true
+      case _ => false
+    }
+  }
+
+  println(" - - - P O O G L E - - -\n")
+
+  val play = Game(Spanish, 7)
+  val dice = play.dice
+
+  println(play.language.welcomeMessage)
+
+  var input = "c"
+
+  while(continue(input)) {
+    val shake: Board = play.shakeTheBox
+    println(play.dibujar(shake))
+    input = scala.io.StdIn.readLine()
+  }
+}
